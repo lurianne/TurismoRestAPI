ae0952ca639597ce969e40d3517390a0d463f569
lurianne <luriannemarianne@gmail.com>
2018-11-07 01:58:20 -0500

    reserva controlador,modelo,repositorio

---
+++ src/main/java/edu/umss/fcyt/TourismRestAPI/module/repositorio/TuristaRepositorio.java
@@ -0,0 +1,7 @@
+package edu.umss.fcyt.TourismRestAPI.module.repositorio;
+import edu.umss.fcyt.TourismRestAPI.module.modelo.Turista;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TuristaRepositorio extends CrudRepository<Turista,Integer> {
+
+}
