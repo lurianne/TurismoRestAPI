8f43bc4ca124f6e881eee2fb18997eab0594a58a
lurianne <luriannemarianne@gmail.com>
2018-11-07 02:03:27 -0500

    clases turistareserva vacias

---
+++ src/main/java/edu/umss/fcyt/TourismRestAPI/module/modelo/TuristaReserva.java
@@ -0,0 +1,4 @@
+package edu.umss.fcyt.TourismRestAPI.module.modelo;
+
+public class TuristaReserva {
+}
---
+++ src/main/java/edu/umss/fcyt/TourismRestAPI/module/repositorio/TuristaReservaRepositorio.java
@@ -0,0 +1,4 @@
+package edu.umss.fcyt.TourismRestAPI.module.repositorio;
+
+public class TuristaReservaRepositorio {
+}
